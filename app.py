dec9669da634f41fec2df058847266610dfbf6bb
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-04-01 12:43:23 -0500

    finished register form and route for new users

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 from forms import RegisterForm
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 
@@ -14,6 +15,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 
 connect_db(app)
+db.create_all()
 
 @app.route('/')
 def home():
@@ -24,4 +26,29 @@ def home():
 def register():
     '''render register template'''
     form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first = form.first_name.data
+        last = form.last_name.data
+        new_user = User.register(username, password, email, first, last)
+        db.session.add(new_user)
+
+        try:
+            db.session.commit()
+        except:
+            form.username.errors.append('Username taken.  Please pick another')
+            return render_template('register.html', form=form)
+
+        session['username'] = new_user.username
+        flash('Welcome! Successfully created account!', 'success')
+        return redirect('/secret')
+
     return render_template('register.html', form = form)
+
+
+@app.route('/secret')
+def secret_page():
+    '''render secret page'''
+    return render_template('secret.html')
--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 
-
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
 
@@ -21,3 +21,24 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable = False, unique = True)
     first_name = db.Column(db.String(30), nullable = False)
     last_name = db.Column(db.String(30), nullable = False)
+
+    @classmethod
+    def register(cls, username, pwd, email, first, last):
+        """Register user w/hashed password & return user."""
+        hashed = bcrypt.generate_password_hash(pwd)
+        # turn bytestring into normal (unicode utf8) string
+        hashed_utf8 = hashed.decode("utf8")
+        # return instance of user w/username and hashed pwd
+        return cls(username=username, password=hashed_utf8, email = email, first_name = first, last_name = last)
+
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Validate that user exists & password is correct.
+        Return user if valid; else return False.
+        """
+        u = User.query.filter_by(username=username).first()
+        if u and bcrypt.check_password_hash(u.password, pwd):
+            # return user instance
+            return u
+        else:
+            return False
